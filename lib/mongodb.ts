767610947bbc55ab95454a246b7521215cbfb6ee
ahmmadSumon <arifahmmadsumon@gmail.com>
2025-12-18 17:56:10 +0600

    das

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
+  console.warn("⚠️ MONGODB_URI not defined");
 }
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
@@ -12,8 +12,8 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
+    cached.promise = mongoose.connect(MONGODB_URI as string).then((mongoose) => {
+      return mongoose;
     });
   }
 
